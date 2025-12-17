983efd78fcd91460f4549e6791f24271bf8e39b0
MohamedAbdalhalem <mohamed3ab7alem@gmail.com>
2025-12-17 21:45:55 +0200

    add submit modal

--- Js/script.js
+++ Js/script.js
@@ -1,10 +1,22 @@
 const collapseBtn = document.querySelector('.collapse-btn')
 const links = document.querySelector('.Links')
-console.log(collapseBtn, links)
+const submitButton = document.querySelector('form button')
+const submitModal = document.querySelector('#submit-modal')
+const submitModalChild = document.querySelector('#submit-modal div')
+console.log(submitButton, submitModal)
 collapseBtn.addEventListener('click', () => {
     if (links.classList.contains('d-none')) {
         links.classList.replace('d-none','d-block')
     } else {
         links.classList.replace('d-block','d-none')
     }
+})
+submitButton.addEventListener('click', () => {
+    submitModal.classList.replace('d-none','d-flex')
+})
+submitModal.addEventListener('click', (e) => {
+    submitModal.classList.replace('d-flex', 'd-none')
+})
+submitModalChild.addEventListener('click', (e) => {
+    e.stopPropagation()
 })
